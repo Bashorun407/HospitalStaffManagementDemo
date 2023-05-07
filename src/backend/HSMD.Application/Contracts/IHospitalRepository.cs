02cb75e22cb8d3b9c930f55fcd345baf74e41307
Bashorun407 <83551336+Bashorun407@users.noreply.github.com>
2023-05-07 13:04:13 +0100

    HSMD.Application class library has  two folders which are CommonContract and Contracts. These folders will contain interfaces that will help with queries

---
+++ src/backend/HSMD.Application/Contracts/IHospitalRepository.cs
@@ -0,0 +1,20 @@
+﻿using HSMD.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSMD.Application.Contracts
+{
+    public interface IHospitalRepository
+    {
+        Task<IEnumerable<Hospital>> GetHospitals(bool trackChanges);
+        Task<Hospital> GetHospitalById(long Id, bool trackChanges);
+        Task<Hospital> GetHospitalByName(string Name, bool trackChanges);
+        void CreateHospital(Hospital hospital);
+        //void UpdateHospital(Hospital hospital); //This may not be needed as 'save' functionality can do same
+        void DeleteHospital(Hospital hospital);
+
+    }
+}
---
+++ src/backend/HSMD.Application/Contracts/IStaffRepository.cs
@@ -0,0 +1,19 @@
+﻿using HSMD.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSMD.Application.Contracts
+{
+    public interface IStaffRepository
+    {
+        Task<IEnumerable<Staff>> GetAllStaffAsync(bool trackChanges);
+        Task<Staff> GetStaffById(long id, bool trackChanges);
+        Task<Staff> GetStaffByName(string firstName, string lastName, bool trackChanges);
+        void CreateStaff(Staff staff);
+        void DeleteStaff(Staff staff);
+    }
+}
+
